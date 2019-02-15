d6db40fc4ea4c637f6a6e5530aaf059106b3cda6
TU4NG4L4U <46773642+TU4NG4L4U@users.noreply.github.com>
2019-02-15 17:46:04 +0700

    Add files via upload

---
+++ TU4N_GALAU_ID.sh
@@ -0,0 +1,371 @@
+#Mau Ngapain
+#Mau Recode Yah
+#Mikir Tolol
+#Recoded By TUAN GALAU
+
+clear
+
+figlet -f slant TU4N G4L4U | lolcat
+
+echo
+echo
+echo "🔴⬜⬜⬜⬜⬜⬜⬜⬜⬜⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬜⬜⬜⬜⬛⬛⬛⬛🔵"
+echo "💻Tools By : TU4N G4L4U💻" | lolcat
+echo "📛Grub : TERMUX TEAM CYBER📛" | lolcat
+echo "🔰WA : 081514074360🔰" | lolcat
+echo "🌀Thanks : Buat Yg Make Tools Saya🌀" | lolcat
+echo "🔵⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬜⬜⬜⬜⬜⬜⬜⬜⬛⬛⬛⬜⬜⬜⬛⬛🔵"
+echo
+echo
+echo ""
+echo
+echo
+echo "⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛"
+echo "Hack FB " | lolcat
+echo "⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛"
+echo "📛1.FB Crackv 1" | lolcat
+echo "📛2.MBF " | lolcat
+echo "📛3.Crack FB 2 " | lolcat
+echo "📛4.crack FB 3 " | lolcat
+echo "📛5.Crack FB 4 " | lolcat
+echo "⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛"
+echo
+echo
+echo "⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜"
+echo "BOT " | lolcat
+echo "⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜"
+echo "📛6.SpiderBot " | lolcat
+echo "📛7.Bot Komen FB" | lolcat
+echo "📛8.Auto Like FB "| lolcat
+echo "📛9.Kumpulah Bot FB " | lolcat
+echo "⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜"
+echo
+echo
+echo "⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛"
+echo "Kumpulan Tools Installer" | lolcat
+echo "⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛"
+echo "📛10.Tools Sk4ter" | lolcat
+echo "📛11.B4J1N64N " | lolcat
+echo "📛12.TU4N G4L4U " | lolcat
+echo "⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛"
+echo
+echo
+echo "⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜"
+echo "Spam Tools " | lolcat
+echo "⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜"
+echo "📛13.Spam TELKOMSEL " | lolcat
+echo "📛14.Tools Spam " | lolcat
+echo "📛15.GcoSpam " | lolcat
+echo "⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜"
+echo
+echo
+echo "⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛"
+echo "Cara Bikin Deface " | lolcat
+echo "⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛"
+echo "📛16.Creator Deface" | lolcat
+echo "📛17.LITETOOLS" | lolcat
+echo "⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛"
+echo
+echo
+echo "⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜"
+echo "WebDav" | lolcat
+echo "⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜"
+echo "📛18.WebDav 1 " | lolcat
+echo "📛19.WebDav 2 " | lolcat
+echo "⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜"
+echo
+echo
+echo "(+)====================================================(+) " | lolcat
+echo "(--)Thanks Friends : MR.17 | MR.EXSTRIME66 | Sk4ters " | lolcat
+echo "(+)====================================================(+)" | lolcat
+echo
+echo
+echo ""
+echo ""
+echo "\033[34;1m  99\033[37;1m.\033[32;1mKeluar\033[37;1m"
+echo ""
+echo "\033[32;1m ──> \033[32;1m Pilih pilih aja gk usah
+komen!\033[32;1m"
+read -p " root@TU4NG4L4U : " pilih
+if [ $pilih = 1 ]
+then
+clear
+figlet TUAN GALAU | lolcat
+echo
+echo "Loading...!!"
+echo ""
+sleep 1
+apt install git
+pkg install python2
+pip2 install mechanize
+git clone https://github.com/rickyricko302/fb-crackv1
+cd fb-crackv1
+python2 fb-crack.py
+fi
+
+if [ $pilih = 2 ]
+then
+clear
+figlet TUAN GALAU | lolcat
+echo
+echo "Loading...!!!"
+echo ""
+sleep 1
+pip2 install mechanize
+pkg install git
+git clone https://github.com/pirmansx/mbf
+cd mbf
+python2 MBF.py
+fi
+
+if [ $pilih = 3 ]
+then
+clear
+figlet TUAN GALAU | lolcat
+echo ""
+echo "Loading..!!!"
+sleep 1
+apt update && apt upgrade
+apt install python2
+apt install python2-dev
+pip2 install mechanize
+curl -S https://pastebin.com/raw/yUpg8zsv --output fb.py
+python2 fb.py
+fi
+
+if [ $pilih = 4 ]
+then
+clear
+figlet TUAN GALAU | lolcat
+echo ""
+echo "Loading...!!"
+echo ""
+sleep 1
+pkg install git
+git clone https://github.com/hnov7/mbf
+cd mbf
+python2 mbf.py
+fi
+
+if [ $pilih = 5 ]
+then
+clear
+figlet TUAN GALAU | lolcat
+echo
+echo "Jika Ingin Berhenti"
+echo "tekan 'ctrl + c "
+sleep 1
+pkg install cmatrix
+cmatrix
+fi
+
+if [ $pilih = 6 ]
+then
+clear
+figlet TUAN GALAU | lolcat
+echo
+echo "Loding..."
+sleep 1
+apt update && apt upgrade
+apt install php
+apt install git
+git clone https://github.com/Cvar1984/SpiderBot.git
+cd SpiderBot
+echo ""
+echo "\033[36;1m"
+echo "  Tinggal DiJalankan Dech:)"
+echo "  Contoh Saya Ingin Pakai Botkomen"
+echo "  Ketik: php botkoment.php"
+echo ""
+ls
+fi
+
+if [ $pilih = 7 ]
+then
+clear
+figlet TUAN GALAU | lolcat
+echo
+echo "Loding.....!!!"
+echo
+sleep 1
+apt install git
+git clone https://github.com/Senitopeng/BotFbBangDjon.git
+cd BotFbBangDjon
+python2 bangdjon.py
+fi
+
+if [ $pilih = 8 ]
+then
+clear
+figlet TUAN GALAU | lolcat
+echo
+echo "Loding......!!!!"
+echo
+sleep 1
+pkg install curl
+apt instal git php curl -y
+apt install git
+git clone http://github.com/AMVengeance/FB-React.git
+cd FB-React
+chmod +x start
+./start
+fi
+
+if [ $pilih = 9 ]
+then
+clear
+figlet TUAN GALAU | lolcat
+echo
+echo "Loding......!!!!"
+echo
+sleep 1
+apt install git
+git clone https://github.com/Senitopeng/BotFbBangDjon.git
+cd BotFbBangDjon
+python2 bangdjon.py
+fi
+
+if [ $pilih = 10 ]
+then
+clear
+figlet TUAN GALAU | lolcat
+echo
+echo "Loding....!!!!"
+echo
+sleep 1
+pkg install git
+git clone https://github.com/Sk4ters38/SK4T3R5v1
+cd SK4T3R5v1
+sh SK4T3RS.sh
+fi
+
+if [ $pilih = 11 ]
+then
+clear
+echo ""
+echo "Loding.......!!!"
+echo
+sleep 1
+apt update && apt upgrade
+pkg install git
+git clone https://github.com/DarknessCyberTeam/B4J1N64Nv5
+cd B4J1N64Nv5
+sh B4J1N64N.sh
+fi
+
+if [ $pilih = 12 ]
+then
+clear
+echo ""
+echo "Loding.....!!!"
+echo
+sleep 1
+pkg install python2
+pkg install git
+git clone https://github.com/TU4NG4L4U/InstallerV6
+cd InstallerV6
+sh InstallerV6.sh
+fi
+
+if [ $pilih = 13 ]
+then
+clear
+figlet TUAN GALAU | lolcat
+echo ""
+echo "Loding....!!!"
+echo
+sleep 1
+pkg install php && pkg install git
+git clone https://github.com/Thaniiaa/b
+cd b
+php dg.php
+fi
+
+if [ $pilih = 14 ]
+then
+clear
+figlet TUAN GALAU |  lolcat
+echo ""
+echo "Loding...!!"
+echo
+sleep 1
+apt install git
+git clone https://github.com/Amriez/gcospam
+cd gcospam
+sh instll.sh
+sh gco.sh
+fi
+
+if [$pilih = 15 ]
+then
+clear
+figlet TUAN GALAU | lolcat
+echo ""
+echo "Loding......!!!"
+echo
+sleep 1
+pkg install git
+git clone https://github.com/Amriez/gcospam
+cd gcospam
+sh install.sh
+sh gco.sh
+fi
+
+if [ $pilih = 16 ]
+then
+clear
+figlet TUAN GALAU | lolcat
+echo
+echo "Loding.....!!!"
+echo
+sleep 1
+pkg install git
+git clone https://github.com/Ubaii/script-deface-creator
+cd script-deface-creator
+chmod +x create.py
+python2 create.py
+fi
+
+if [ $pilih = 17 ]
+then
+clear
+figlet TUAN GALAU | lolcat
+echo
+echo "Loding....!!"
+echo
+sleep 1
+pkg install git
+git clone https://github.com/4L13199/LITESCRIPT
+cd LITESCRIPT
+python2 LITESCRIPT.py
+fi
+
+if [ $pilih = 18 ]
+then
+clear
+figlet TUAN GALAU | lolcat
+echo
+echo "Loding.....!!!!"
+echo
+sleep 1
+pkg install git
+git clone https://github.com/TU4NG4L4U/DrakNet
+cd DrakNet
+sh DrakNet.sh
+fi
+
+if [ $pilih = 19 ]
+then
+clear
+figlet TUAN GALAU | lolcat
+echo ""
+echo "Loding...............!!!!"
+echo
+sleep 1
+pkg install git
+git clone https://github.com/TUANB4DUT/WEBDAV
+cd WEBDAV
+sh webdav.sh
+fi
+
+if
